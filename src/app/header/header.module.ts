712ff5172ed923d841e63bc8d32269958aa6a04a
Renan Ribiro Brando <reebrando@gmail.com>
2020-01-20 21:10:02 -0300

    Created header component and added styles.

---
+++ src/app/header/header.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header.component';
+
+@NgModule({
+  declarations: [,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [HeaderComponent]
+})
+export class HeaderModule { }
