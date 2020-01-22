9233d379bfbb6ce2f9fcd4dfdb7710a237147c29
Renan Ribiro Brando <reebrando@gmail.com>
2020-01-21 21:56:05 -0300

    Added email form, and push to emails list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Titulo da sua página atual';
+  title: String;
+  private _isNewEmailOpen: Boolean;
+  get isNewEmailOpen () {
+    return this._isNewEmailOpen
+  }
+  emailList: Array<Object>;
+  email: Object;
+
+  constructor() {
+    this.title = 'Titulo da sua página atual';
+    this._isNewEmailOpen = false;
+    this.email = {
+      para: '',
+      assunto: '',
+      conteudo: ''
+    }
+    this.emailList = []
+  }
+
+  toggleEmail() {
+    this._isNewEmailOpen = !this._isNewEmailOpen;
+  }
+
+  setValue(event, target) {
+    this.email[target] = event.target.value;
+    console.log(this.email);
+  }
+  
+  handleEmail (form: NgForm) {
+    console.log(form);
+    
+    if (form.invalid) return;
+    console.log(this.email);
+    
+    // event.preventDefault()
+    this.emailList.push(this.email)
+    
+    this.email = {
+      para: '',
+      assunto: '',
+      conteudo: ''
+    };
+
+    form.reset();
+    this.toggleEmail()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from '../app/header/header.component'
+import { HeaderComponent } from '../app/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { HeaderComponent } from '../app/header/header.component'
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
